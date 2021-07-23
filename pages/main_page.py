1df2a48c6ebfd4c7a3c527255da3d902ff5233d5
Dmitry Marichev <mrchv@ya.ru>
2021-07-23 11:37:23 +0300

    Добавлен  тест с проверкой формы логина и регистрации

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,10 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        login_link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
